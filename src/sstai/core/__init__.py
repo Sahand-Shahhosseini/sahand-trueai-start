0283b49090bbd82742b5a2df78ac034d711c7340
Sahand <sahandshahhosseini@gmail.com>
2025-05-29 19:01:22 +0330

    Add Sahand Knapsack algorithm and API

--- src/sstai/core/__init__.py
+++ src/sstai/core/__init__.py
@@ -1,5 +1,6 @@
 from .fractal import compute_fractal
 from .field import normalize
 from .lemmas import load_lemmas
+from .knapsack import sahand_knapsack
 
-__all__ = ["compute_fractal", "normalize", "load_lemmas"]
+__all__ = ["compute_fractal", "normalize", "load_lemmas", "sahand_knapsack"]
